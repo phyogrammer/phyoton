9ebaab8e692d38a486c95415cfac39f8f6c76b97
phyo <sphyokha@gmail.com>
2026-01-03 16:21:35 -0800

    Setup the skeleton of the application, some data structures, memory management, simple debugging

---
+++ chunk.h
@@ -0,0 +1,22 @@
+#ifndef phyoton_chunk_h
+#define phyoton_chunk_h
+
+#include "common.h"
+
+typedef enum
+{
+    OP_RETURN, // returns from the current function
+} OpCode;
+
+typedef struct
+{
+    int count;
+    int capacity;
+    uint8_t *code;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
+
+#endif
---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef phyoton_debug_h
+#define phyoton_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+int disassembleInstruction(Chunk *chunk, int offset);
+
+#endif
